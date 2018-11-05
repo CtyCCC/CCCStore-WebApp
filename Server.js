63943308f0c9cf52b7f474efd6b479923058bdf3
Nguyễn Văn Mạnh Cường <cuongfbi1314@gmail.com>
2018-11-05 18:46:00 +0700

    Tìm theo khoảng giá+ fix 1 số lỗi nhỏ bên trang cart

--- Server.js
+++ Server.js
@@ -4,7 +4,6 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 var AWS = require('aws-sdk');
 var url = require('url');
-var request = require('request');
 
 /////Khai báo config cho aws sử dụng dynamodb
 AWS.config.update({
@@ -360,51 +359,82 @@ app.get('/phukien',function (req,res) {
 
 //////Product/Tim theo giá/////
 app.get("/search",function (req,res) {
+    var route = url.parse(req.url,true).query;
+    var upper = Number(route.upper)*1000000;
+    var lower = Number(route.lower)*1000000;
 
+    console.log(upper);
+    console.log(lower);
 
-    // var $ = cheerio.load(body);
-    // var a = $("#value-upper").text();
-    // var b = $("#value-lower").text();
-    // var pathname = url.parse(req.url,true).pathname;
-    // console.log();
-    // console.log(a);
-    // console.log(b);
-    // console.log(pathname);
+    var params = {
+        TableName:"Product",
+        ProjectionExpression:"nameSP , info",
+        FilterExpression: "info.price between :p1 and :p2",
+        ExpressionAttributeValues:{
+            ":p1": lower,
+            ":p2": upper,
+        }
+    };
+    var index_sp = 1;
+    docClient.scan(params, onScan);
+    function onScan(err, data) {
+        if (err) {
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Scan succeeded.");
+            fs.readFile(__dirname+"/product.html",'utf8',function (err,data1) {
 
-    // var params = {
-    //     TableName:"Product",
-    //     ProjectionExpression:"nameSP , info",
-    //     FilterExpression: "info.price between :p1 and :p2",
-    //     ExpressionAttributeValues:{
-    //         ":p1": "10",
-    //         ":p2": "17090000",
-    //     }
-    // };
-    // docClient.scan(params, onScan);
-    //
-    // function onScan(err, data) {
-    //     if (err) {
-    //         console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
-    //     } else {
-    //         // print all the movies
-    //         console.log("Scan succeeded.");
-    //         data.Items.forEach(function(item) {
-    //             console.log("-Name:"+item.nameSP+"-Gia:"+item.info.price);
-    //         });
-    //         if (typeof data.LastEvaluatedKey != "undefined") {
-    //             console.log("Scanning for more...");
-    //             params.ExclusiveStartKey = data.LastEvaluatedKey;
-    //             docClient.scan(params, onScan);
-    //         }
-    //     }
-    // }
+                var $ = cheerio.load(data1);
+
+                //cái này cho 2 cái banner
+                var banner1 = '#banner11';
+                var banner2 = '#banner22';
+                //gán cứng tạm, sau này thêm data cho 2 cái banner,giờ làm biếng tạo s3 quá, có 3 tấm hình
+                //tại nếu có để thì phải mua đc, chứ bấm vào lại ko có gì
+                $(banner1 + "").attr('href',"product-detail?id=LT005");
+                $(banner2 + "").attr('href',"product-detail?id=LK004");
+
+                //---Load box chứa item
+                for(var i=2;i<data.Items.length;i++)
+                {
+                    var product = $('#boxstart').clone();
+                    product.removeAttr('id');
+                    product.find('#img_sp_1').attr('id','img_sp_'+i);
+                    product.find('#name_sp_1').attr('id','name_sp_'+i);
+                    product.find('#price_sp_1').attr('id','price_sp_'+i);
+                    product.appendTo('.row_product');
+                }
+
+                data.Items.forEach(function(product){
+                    //console.log("id:"+ product.idSP + "  +tenSP:", product.nameSP);
+                    var namesp = '#name_sp_';
+                    var giasp = '#price_sp_';
+                    var imgsp = '#img_sp_';
+                    //console.log($(imgsp + index_sp).attr('src'));
+                    $(namesp + index_sp + "").text(product.nameSP);
+                    $(giasp + index_sp + "").text(product.info.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")+" VNĐ");
+                    $(imgsp + index_sp).attr('src', product.info.images[0]);
+                    $(namesp + index_sp + "").attr('href',"product-detail?id="+product.idSP);
+                    $(giasp + index_sp + "").attr('aria-valuetext',product.info.price);
+                    index_sp = index_sp + 1;
+                });
+                res.writeHead(200,{'Context-Type':'text/html'});
+                res.write($.html());
+                res.end();
+            });
+            if (typeof data.LastEvaluatedKey != "undefined") {
+                console.log("Scanned all data...");
+                params.ExclusiveStartKey = data.LastEvaluatedKey;
+                docClient.scan(params, onScan);
+            }
+        }
+    }
 });
 
 
 ///Get trang product-detail///////
 app.get('/product-detail',function (req,res) {
     var route = url.parse(req.url,true).query;
-    console.log(route);
     var id = route.id;
     var params = {
         TableName : "Product",
@@ -425,7 +455,8 @@ app.get('/product-detail',function (req,res) {
                 var $ = cheerio.load(data1);
                 data.Items.forEach(function(item) {
                     $('#name_sp').text(item.nameSP);
-                    $('#price_sp').text(item.info.price+" VNĐ");
+                    $('#price_sp').text(item.info.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")+" VNĐ");
+                    $('#price_sp').attr('aria-valuetext',item.info.price);
                     $('#img_1_1').attr('data-thumb',item.info.images[0]);
                     $('#img_1_2').attr('src',item.info.images[0]);
                     $('#img_2_1').attr('data-thumb',item.info.images[1]);
