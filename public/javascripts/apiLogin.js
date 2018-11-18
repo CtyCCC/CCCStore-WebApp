d7f611eb6b44bfc186d533037ce07518059f7699
Cuong <hungcuongkai123@gmail.com>
2018-11-18 18:43:20 +0700

    login

--- public/javascripts/apiLogin.js
+++ public/javascripts/apiLogin.js
@@ -2,34 +2,31 @@
 =======================================================*/
 window.fbAsyncInit = function() {
 		FB.init({
-		appId      : '322620955226186',
+		appId      : '495449644277627',
 		cookie     : true,
 		xfbml      : true,
 		version    : 'v3.2'
 	});
 
-	function fb_login() {
-  		FB.login( function() {}, { scope: 'email,public_profile' } );
-	}
-	FB.getLoginStatus(function(response) {
-    	statusChangeCallback(response);
-	});	      
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+    FB.logout(function(response) {
+        // user is now logged out
+    });
+
 };
 
 (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.2&appId=322620955226186&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.2&appId=322620955226186&autoLogAppEvents=1';
+    fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 /// login thanh công lấy dữ liệu gì lấy từ response
 function statusChangeCallback(response){
-	if(response=='connected')
-		console.log('Thanh Cong');
-	else
-		console.log('That bai');
 }
 /*[******]
 =======================================================*/
